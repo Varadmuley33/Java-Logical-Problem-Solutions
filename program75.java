8c32b3d9b8b400acf57ef1a08cbb3a79a16b69a9
Varad Muley <varad.muley408@gmail.com>
2026-01-18 23:24:49 +0530

    Update program75.java

---
+++ program75.java
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
+import java.io.*;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Class Name  : program75
+//  Description : It is used to display factors of a given number using BufferedReader with try-catch
+//  Author      : Varad Nitin Muley
+//  Date        : 09/10/2025
+//
+/////////////////////////////////////////////////////////////////
+
+public class program75
+{
+    /////////////////////////////////////////////////////////////////
+    //
+    //  Entry point function for the application
+    //
+    /////////////////////////////////////////////////////////////////
+
+    public static void main(String A[])
+    {
+        int iNo = 0;                                            // To accept user input
+        int i = 0;                                              // Loop counter
+
+        BufferedReader bobj = new BufferedReader(
+                                new InputStreamReader(System.in)); // BufferedReader object
+
+        System.out.println("Enter number : ");
+
+        try
+        {
+            iNo = Integer.parseInt(bobj.readLine());            // Accept number
+        }
+        catch(IOException iobj)                                 // Exception handling
+        {
+        }
+
+        for(i = 1; i <= (iNo/2); i++)                           // Loop from 1 to half of iNo
+        {
+            if((iNo % i) == 0)                                  // Check factor condition
+            {
+                System.out.println(i);                          // Print factor
+            }
+        }
+    }   // End of main
+}   // End of program75
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handaled by the application
+//
+//  Input  : 12     Output : 1 2 3 4 6
+//  Input  : 10     Output : 1 2 5
+//  Input  : 15     Output : 1 3 5
+//
+/////////////////////////////////////////////////////////////////
