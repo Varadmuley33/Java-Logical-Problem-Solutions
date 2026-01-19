a1f5d9868ae60440422ee41536517719711ca75c
Varad Muley <varad.muley408@gmail.com>
2026-01-19 23:39:08 +0530

    Add files via upload

---
+++ program104.java
@@ -0,0 +1,96 @@
+
+////////////////////////////////////////////////////////////
+//
+//  File name :     program102.java
+//  Description :   To return sum of elemts of array 
+//  Author :        Pallavi Sable
+//  Date :          7/11/2025
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+//
+//  REQUIRED HEADER FILES
+//
+////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+////////////////////////////////////////////////////////////
+//
+//  Function Name : Summation
+//  Description :   returns sum of elements of array
+//  Intput :        int
+//  Output :        int
+//  Author :        Pallavi Sable
+//  Date :          07/11/2025
+//  Time Complexity:O(N)
+//               
+////////////////////////////////////////////////////////////
+
+class ArrayX
+{
+    public int Arr[];
+    public int iSize;                           //Characteristics
+
+    public ArrayX(int iNo)
+    {
+        System.out.println("Inside Constructor");
+        iSize = iNo;
+        Arr = new int[iSize];                   //allocated resource
+        
+    }
+    public void Accept()                        //To take input
+    {
+        int iCnt = 0;
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the elements of array:");
+        
+        for(iCnt = 0 ; iCnt < Arr.length ; iCnt++)
+        {
+            iSize = sobj.nextInt();
+        }
+
+    }
+    public void Display()                       //To Display Output
+    {
+        int iCnt = 0;
+        System.out.println("elements of the array are:");
+        for(iCnt = 0; iCnt < Arr.length ;iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+
+    }
+}
+
+
+////////////////////////////////////////////////////////////
+//
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+//
+////////////////////////////////////////////////////////////
+
+class program104
+{
+    public static void main (String A[])
+    {
+        ArrayX aobj1 = new ArrayX(5);
+        ArrayX aobj2 = new ArrayX(7);
+        
+    }
+}
+
+
+//End of main
+
+////////////////////////////////////////////////////////////
+//
+// Test cases succesfully handled by the application
+// Input1 : 5       Output : 
+// Input1 : 15      Output : false
+// Input1 : -28     Output : true
+// Input1 : 11      Output : false
+// Input1 : 24      Output : false
+//
+////////////////////////////////////////////////////////////
