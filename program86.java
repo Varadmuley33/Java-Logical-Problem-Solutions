92bf3ec6c745ba10ecd7ede9b8ce90a5ee1aef24
Varad Muley <varad.muley408@gmail.com>
2026-03-19 00:10:09 +0530

    Update program86.java

---
+++ program86.java
@@ -0,0 +1,52 @@
+// Factorial code 
+// Input : 5 
+// Output 1   *   2   *   3   *   4   *   5 = 120
+import java.util.*;
+
+class Number
+{
+    public int CalculateFactorial(int iNo)
+    {
+        int i = 0 ;
+        int iFact = 1;
+
+        for(i = 1 ; i<= iNo ; i++)
+        {
+            iFact = iFact * i;
+        }
+        return iFact;
+
+    }
+
+}// End of number 
+
+
+public class program86
+{
+
+    public static void main (String A[])
+    {
+        Scanner sobj = new Scanner (System.in);
+
+        int iValue = 0 ;
+        int iRet = 0;
+
+        
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+
+
+        iRet = nobj.CalculateFactorial(iValue); 
+        System.out.println("Factorial is : " + iRet);
+
+       
+
+        // Important 
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+    }
+}
