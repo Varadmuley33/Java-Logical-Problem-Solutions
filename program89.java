e3883d23806f6bd354d7fb43bdcfb84722349185
Varad Muley <varad.muley408@gmail.com>
2026-03-19 00:10:58 +0530

    Update program89.java

---
+++ program89.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Number
+{
+    public long CalculateFactorial(int iNo)
+    {
+        int i = 0 ;
+        long iFact = 1;   //Important
+
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+
+        i = 1;
+
+        while(i <= iNo)              // Change 
+        {
+            iFact = iFact * i;
+            i++;
+        }
+        return iFact;
+
+    }
+
+}// End of number 
+
+
+public class program89
+{
+
+    public static void main (String A[])
+    {
+        Scanner sobj = new Scanner (System.in);
+
+        int iValue = 0 ;
+        long iRet = 0;
+
+        
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+
+
+        iRet = nobj.CalculateFactorial(iValue); 
+        System.out.println("Factorial is : " + iRet);
+
+       
+
+        // Important 
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+    }
+}
