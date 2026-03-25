e2bf14f59bfb86b551d7e8f13b3182324d3cc437
Varad Muley <varad.muley408@gmail.com>
2026-03-25 23:21:51 +0530

    Update program10.java

--- program10.java
+++ program10.java
@@ -1,1 +1,12 @@
 
+class program109
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {10,20,30,40};
+
+        System.out.println(Arr);
+        System.out.println(Arr);
+        System.out.println(Arr);
+    }
+}
