caaed95ca74114df35747fd24cd5822b34ea2d86
Varad Muley <varad.muley408@gmail.com>
2026-03-25 23:23:25 +0530

    Update program111.java

---
+++ program111.java
@@ -0,0 +1,14 @@
+class program111
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        int Arr[] = {10,20,30,40};
+
+        for(iCnt = 0; iCnt < 4; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+    }
+}
