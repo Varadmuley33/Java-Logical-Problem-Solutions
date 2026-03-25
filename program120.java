4c41159c20bc2af593e97b1bcd9cb19300c65e4c
Varad Muley <varad.muley408@gmail.com>
2026-03-25 23:30:11 +0530

    Update program120.java

---
+++ program120.java
@@ -0,0 +1,19 @@
+class program120
+{
+    public static void Display(int Arr[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 0; iCnt < 7; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+    }
+
+    public static void main(String A[])
+    {
+        int Brr[] = {10,20,30,40,50,60,70};
+
+        Display(Brr);
+    }
+}
