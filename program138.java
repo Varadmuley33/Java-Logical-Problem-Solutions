eaafe2a1fb08036dd5fecc9a8c57039d274923f0
Varad Muley <varad.muley408@gmail.com>
2026-05-13 23:41:18 +0530

    Implement linear search in Program138.java
    This program implements a linear search algorithm to find a user-entered number in an array. It includes memory allocation for the array and handles user input.

---
+++ program138.java
@@ -0,0 +1,86 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program138.java
+//  Description :   Searches a user entered number from the array using
+//                  Linear Search with break statement optimization.
+//  Author :        Varad Nitin Muley
+//  Date :          14/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Program138
+{
+    // O(N)
+    public static boolean LinearSearch(int Arr[], int iSize, int iNo)
+    {
+        int iCnt = 0, iCount = 0;
+
+        for(iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            if(Arr[iCnt] == iNo)
+            {
+                iCount++;
+                break;
+            }
+        }
+
+        if(iCount > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iLength = 0, iCnt = 0, iValue = 0;
+        boolean bRet = false;
+
+        int iPtr[];
+
+        System.out.println("Enter the number of elements : ");
+        iLength = sobj.nextInt();
+
+        // Step 1 : Allocate the memory
+        iPtr = new int[iLength];
+
+        System.out.println("Enter the values : ");
+
+        for(iCnt = 0; iCnt < iLength; iCnt++)
+        {
+            iPtr[iCnt] = sobj.nextInt();
+        }
+
+        System.out.print("Enter the number you want to search : ");
+        iValue = sobj.nextInt();
+
+        bRet = LinearSearch(iPtr, iLength, iValue);
+
+        if(bRet == true)
+        {
+            System.out.println(iValue + " is present in given elements");
+        }
+        else
+        {
+            System.out.println(iValue + " is not present in given elements");
+        }
+
+        // Step 3 : Free the memory
+        iPtr = null;
+
+        sobj.close();
+    }
+}
