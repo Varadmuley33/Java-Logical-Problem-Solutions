3b3579e0e5f0d4246d2fc0aa27ebb48a6bae863c
Varad Muley <varad.muley408@gmail.com>
2026-05-14 23:02:27 +0530

    Implement maximum number finder in Program146
    This program finds and displays the maximum number from the entered array elements using combined initialization.

---
+++ program146.java
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program146.java
+//  Description :   Finds and displays the maximum number from the
+//                  entered array elements using combined initialization.
+//  Author :        Varad Nitin Muley
+//  Date :          14/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Program146
+{
+    public static int Maximum(int Arr[], int iSize)
+    {
+        int iMax = 0;
+        int iCnt = 0;
+
+        for(iMax = Arr[iCnt], iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            if(Arr[iCnt] > iMax)
+            {
+                iMax = Arr[iCnt];
+            }
+        }
+
+        return iMax;
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iLength = 0, iCnt = 0, iRet = 0;
+
+        int iPtr[];
+
+        System.out.println("Enter the number of elements : ");
+        iLength = sobj.nextInt();
+
+        // Step 1 : Allocate the memory
+        iPtr = new int[iLength];
+
+        System.out.println("Enter the values : ");
+
+        for(iCnt = 0; iCnt < iLength; iCnt++)
+        {
+            iPtr[iCnt] = sobj.nextInt();
+        }
+
+        iRet = Maximum(iPtr, iLength);
+
+        System.out.println("Maximum number is : " + iRet);
+
+        // Step 3 : Free the memory
+        iPtr = null;
+
+        sobj.close();
+    }
+}
