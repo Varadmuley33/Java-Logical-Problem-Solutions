f16ce306cd34f023e1d41aba9dfdaa58c5924f62
Varad Muley <varad.muley408@gmail.com>
2026-07-04 21:37:37 +0530

    Update program128.java

--- program128.java
+++ program128.java
@@ -2,7 +2,7 @@
 //
 //  File name :     Program128.java
 //  Description :   Calculates summation of array elements using loop.
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley   
 //  Date :          13/11/2025
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
