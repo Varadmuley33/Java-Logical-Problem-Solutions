b08afc1a4e7622dde560ddf33dd1466dadfcfc83
Varad Muley <varad.muley408@gmail.com>
2026-08-27 23:53:12 +0530

    Remove redundant comments and clean up code

--- program141.java
+++ program141.java
@@ -3,7 +3,7 @@
 //  File name :     Program141.java  
 //  Description :   Searches a user entered number from the array using
 //                  Linear Search with direct condition return.
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley  
 //  Date :          14/11/2025   
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
