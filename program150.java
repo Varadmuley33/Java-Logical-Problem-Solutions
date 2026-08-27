be28377b5d4259b164561dc02b19691537ad5044
Varad Muley <varad.muley408@gmail.com>
2026-08-27 23:55:37 +0530

    Remove unnecessary comments and clean up code

--- program150.java
+++ program150.java
@@ -2,7 +2,7 @@
 //
 //  File name :     Program150.java
 //  Description :   Displays all array elements in reverse order.  
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley   
 //  Date :          14/11/2025   
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
