851f5b039b55005312c05293a016e4c9652657b5
Varad Muley <varad.muley408@gmail.com>
2026-09-06 23:26:18 +0530

    Remove redundant comments and format code

--- program104.java
+++ program104.java
@@ -2,7 +2,7 @@
 //
 //  File name :     program104.java
 //  Description :   Design the class using OOP design
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley   
 //  Date :          07/11/2025
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
