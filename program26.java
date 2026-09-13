f1943dfccdffe91f290b6802e426bd9aed1f9326
Varad Muley <varad.muley408@gmail.com>
2026-09-13 23:12:12 +0530

    Reformat comments and structure in program26.java

--- program26.java
+++ program26.java
@@ -2,7 +2,7 @@
 //
 //  File name :     program26.java
 //  Description :   Demonstrates dynamic function usage in Java by printing
-//                  a message based on user-entered frequency.
+//                  a message based on user-entered frequency.   
 //  Author :        Varad Nitin Muley
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
